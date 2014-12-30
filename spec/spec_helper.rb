99bb19a0c442e092dff1b27118a06a2d274b427f
Emilien Macchi <emilien.macchi@enovance.com>
2014-12-30 13:06:12 -0500

    Implement openstacklib::wsgi::apache
    Allow to serve a service with apache mod_wsgi like.
    This code will be consummed by OpenStack project that allow to run a
    service as a WSGI process.
    
    Change-Id: Ib606df7d6ceec1db032c8e133598056878df0a01

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'shared_examples'
 require 'vcr'
+
+
+RSpec.configure do |c|
+  c.alias_it_should_behave_like_to :it_configures, 'configures'
+  c.alias_it_should_behave_like_to :it_raises, 'raises'
+end
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr'
