b1ff5ff4bedc49b9e8e691d8fb99fabd75ea98b9
Radoslaw Smigielski <radoslaw.smigielski@nokia.com>
2016-03-09 16:27:12 +0100

    Fix type in os_service_default fact
    Fix a type in path for facter >= 2.0.1, path: facts.d/os_services_default.txt
    does not exist.
    
    Change-Id: I8879bc6ff498776a488e6a62d5d9de06fefa9cfc

--- lib/facter/os_service_default.rb
+++ lib/facter/os_service_default.rb
@@ -1,6 +1,6 @@
 #
 # This adds the os_service_default fact for people with facter < 2.0.1
-# For people with facter >= 2.0.1, the facts.d/os_services_default.txt should
+# For people with facter >= 2.0.1, the facts.d/os_service_default.txt should
 # provide this information
 #
 require 'puppet/util/package'
