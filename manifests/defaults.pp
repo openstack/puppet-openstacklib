f2d197719188d32b832f2f16a4f1722cda391ed2
Tobias Urdin <tobias.urdin@binero.se>
2018-11-05 19:48:37 +0100

    Deprecate Puppet 4
    Adds a message that Puppet 4 is deprecated as of
    this release, is minimally tested and will be
    officially unsupported in the next release.
    
    Change-Id: I5fbd87fc179ee80a749bad02764f129420330f5c

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -5,4 +5,11 @@
 # This file is loaded in the params.pp of each class.
 #
 class openstacklib::defaults {
+
+  # TODO(tobias-urdin): Remove this in the T release when we remove
+  # all Puppet 4 related code.
+  if versioncmp($::puppetversion, '5.0.0') < 0 {
+    warning('OpenStack modules support for Puppet 4 is deprecated \
+and will be officially unsupported in the T release')
+  }
 }
