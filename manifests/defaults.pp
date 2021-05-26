008bcaf8e630e6cbeeeea92b9ee3d95ce62dc232
yatinkarel <ykarel@redhat.com>
2021-05-26 12:58:09 +0530

    Set python versions for rhel > 8
    EL8 was shipped with python 3.6, EL9 is being shipped with
    python 3.9, so let's set python versions accordingly.
    
    Change-Id: I5d857a3fbab2143c490016cd6b9cfc78ecb382e5
    (cherry picked from commit 91bd836fe5fb546efc21a1cc49566ecabcacf21e)

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -9,11 +9,25 @@ class openstacklib::defaults {
   if ($::os['family'] == 'Debian') {
     $pyvers = '3'
     $pyver3 = '3'
-  } elsif ($::os['name'] == 'Fedora') or
-          ($::os['family'] == 'RedHat' and Integer.new($::os['release']['major']) > 7) {
+  } elsif ($::os['name'] == 'Fedora') {
+    # TODO(tkajinam): This is very outdated and doesn't work with the latest
+    #                 Fedora. We should fix or drop this.
     $pyvers = '3'
     $pyver3 = '3.6'
+  } elsif $::os['family'] == 'RedHat' {
+    if Integer.new($::os['release']['major']) > 8 {
+      $pyvers = '3'
+      $pyver3 = '3.9'
+    } elsif Integer.new($::os['release']['major']) == 8 {
+      $pyvers = '3'
+      $pyver3 = '3.6'
+    } else {
+      $pyvers = ''
+      $pyver3 = '2.7'
+    }
   } else {
+    # TODO(tkajinam) This is left to keep the previous behavior but we should
+    #                revisit this later.
     $pyvers = ''
     $pyver3 = '2.7'
   }
