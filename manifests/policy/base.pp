358e2a4aa0c4ad35840048302ace0ac296b32a1a
Jake Yip <jake.yip@ardc.edu.au>
2022-04-08 18:42:20 +1000

    Make regex matching policy key stricter
    There is a bug where this regex can match multiple lines if a policy
    contains ':'.
    
    Make this regex stricter by making sure the key doesn't end with a ':'
    within the quotes.
    
    Closes-Bug: #1968294
    
    Change-Id: I4090d6831db8ddc4fba294f181085b657e2b3345
    (cherry picked from commit 33fb90326fadd59759d4a65dae0ac873e34ee95b)
    (cherry picked from commit 9e10e80160916ea6a93c230a2f6e6d9726227b1a)
    (cherry picked from commit 81ea071166e4396b96a0627c16fb9c2e94d9dd32)

--- manifests/policy/base.pp
+++ manifests/policy/base.pp
@@ -101,7 +101,7 @@ define openstacklib::policy::base (
       file_line { "${file_path}-${key}" :
         path  => $file_path,
         line  => "'${key}': '${value_real}'",
-        match => "^['\"]?${key}['\"]?\\s*:.+"
+        match => "^['\"]?${key}(?!:)['\"]?\\s*:.+"
       }
       File<| title == $file_path |>
       -> File_line<| title == "${file_path}-${key}" |>
