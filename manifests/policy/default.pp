d689704739abac1711403b3379537e80de212d91
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-06-10 13:20:18 +0900

    Remove support for json format policy files
    Json format was deprecated multiple cycles ago in favor of the new
    yaml format.
    
    Change-Id: Ie43d9cc2441bd864e13ec792e1f6ce76dda354cf

--- manifests/policy/default.pp
+++ manifests/policy/default.pp
@@ -5,7 +5,7 @@
 # == Parameters:
 #
 # [*file_path*]
-#   (Optional) Path to the policy.json file
+#   (Optional) Path to the policy file
 #   Defaults to $name
 #
 # [*file_mode*]
@@ -21,9 +21,8 @@
 #   Defaults to undef
 #
 # [*file_format*]
-#   (Optional) Format for file contents. Valid values
-#   are 'json' or 'yaml'.
-#   Defaults to 'json'.
+#   (Optional) Format for file contents. Valid value is 'yaml'.
+#   Defaults to 'yaml'.
 #
 # [*purge_config*]
 #   (Optional) Whether to set only the specified policy rules in the policy
@@ -35,33 +34,15 @@ define openstacklib::policy::default (
   $file_mode                        = '0640',
   $file_user                        = undef,
   $file_group                       = undef,
-  Enum['json', 'yaml'] $file_format = 'json',
+  Enum['yaml'] $file_format         = 'yaml',
   Boolean $purge_config             = false,
 ) {
-
-  case $file_format {
-    'json': {
-      warning('Json format is deprecated and will be removed in a future release')
-      $content = '{}'
-    }
-    'yaml': {
-      if stdlib::extname($file_path) == '.json' {
-        # NOTE(tkajinam): It is likely that user is not aware of migration from
-        #                 policy.json to policy.yaml
-        fail("file_path: ${file_path} should be a yaml file instead of a json file")
-      }
-      $content = ''
-    }
-    default: {
-      fail("${file_format} is an unsupported policy file format. Choose 'json' or 'yaml'.")
-    }
-  }
 
   ensure_resource('file', $file_path, {
     mode    => $file_mode,
     owner   => $file_user,
     group   => $file_group,
     replace => $purge_config,
-    content => $content
+    content => ''
   })
 }
