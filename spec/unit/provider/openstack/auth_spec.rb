1311b4471aab17c3ce1b9372be7f9ebe2e8ae6ee
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-11-07 23:37:38 +0900

    Drop deprecated fallback to openrc
    It was deprecated 3 years ago when we introduced the logic to use
    clouds.yaml [1].
    
    [1] 08bf393ee48723cd344ff5bce37843c41878914c
    
    Depends-on: https://review.opendev.org/966472
    Change-Id: I4d9a97a58791889bee06143ebb63469d7a5fa8be
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- spec/unit/provider/openstack/auth_spec.rb
+++ spec/unit/provider/openstack/auth_spec.rb
@@ -62,12 +62,6 @@ describe Puppet::Provider::Openstack::Auth do
     end
   end
 
-  describe '#rc_filename' do
-    it 'returns RCFILENAME' do
-      expect(klass.rc_filename).to eq("#{ENV['HOME']}/openrc")
-    end
-  end
-
   describe '#get_os_from_env' do
     context 'with Openstack environment variables set' do
       it 'provides a hash' do
@@ -118,70 +112,6 @@ describe Puppet::Provider::Openstack::Auth do
 
         response = klass.get_os_vars_from_cloudsfile('project')
         expect(response).to eq({})
-      end
-    end
-  end
-
-  describe '#get_os_vars_from_rcfile' do
-    context 'with a valid RC file' do
-      it 'provides a hash' do
-        content = "export OS_USERNAME='test'\nexport OS_PASSWORD='abc123'\nexport OS_PROJECT_NAME='test'\nexport OS_AUTH_URL='http://127.0.0.1:5000'"
-        filename = 'file'
-        expect(File).to receive(:exist?).with('file').and_return(true)
-        expect(File).to receive(:open).with('file').and_return(StringIO.new(content))
-
-        response = klass.get_os_vars_from_rcfile(filename)
-        expect(response).to eq({
-          "OS_AUTH_URL"     => "http://127.0.0.1:5000",
-          "OS_PASSWORD"     => "abc123",
-          "OS_PROJECT_NAME" => "test",
-          "OS_USERNAME"     => "test"})
-      end
-    end
-
-    context 'with a valid RC file with extra code in it' do
-      it 'provides a hash' do
-        content = "export OS_USERNAME='test'\nexport OS_PASSWORD='abc123'\nexport OS_PROJECT_NAME='test'\nexport OS_AUTH_URL='http://127.0.0.1:5000'\n_openstack() {\n foo\n} "
-        filename = 'file'
-        expect(File).to receive(:exist?).with('file').and_return(true)
-        expect(File).to receive(:open).with('file').and_return(StringIO.new(content))
-
-        response = klass.get_os_vars_from_rcfile(filename)
-        expect(response).to eq({
-          "OS_AUTH_URL"     => "http://127.0.0.1:5000",
-          "OS_PASSWORD"     => "abc123",
-          "OS_PROJECT_NAME" => "test",
-          "OS_USERNAME"     => "test"})
-      end
-    end
-
-    context 'with an empty file' do
-      it 'provides an empty hash' do
-        filename = 'file'
-        expect(File).to receive(:exist?).with(filename).and_return(true)
-        expect(File).to receive(:open).with(filename).and_return(StringIO.new(""))
-
-        response = klass.get_os_vars_from_rcfile(filename)
-        expect(response).to eq({})
-      end
-    end
-
-    context 'with a nonexistent file' do
-      it 'should get default rcfile when no environment or openrc file' do
-        ENV.clear
-        content = "export OS_USERNAME='user'\nexport OS_PASSWORD='secret'\nexport OS_PROJECT_NAME='project'\nexport OS_AUTH_URL='http://127.0.0.1:5000'"
-        filename = '/root/openrc'
-
-        expect(File).to receive(:exist?).with("#{ENV['HOME']}/openrc").and_return(false)
-        expect(File).to receive(:exist?).with(filename).and_return(true)
-        expect(File).to receive(:open).with(filename).and_return(StringIO.new(content))
-
-        expect(klass.get_os_vars_from_rcfile("#{ENV['HOME']}/openrc")).to eq({
-          'OS_USERNAME'     => 'user',
-          'OS_PASSWORD'     => 'secret',
-          'OS_PROJECT_NAME' => 'project',
-          'OS_AUTH_URL'     => 'http://127.0.0.1:5000'
-        })
       end
     end
   end
@@ -264,59 +194,6 @@ describe Puppet::Provider::Openstack::Auth do
           'OS_IDENTITY_API_VERSION' => '3',
           'OS_CLOUD'                => 'project',
           'OS_CLIENT_CONFIG_FILE'   => '/etc/openstack/puppet/clouds.yaml',
-        })
-      end
-    end
-
-    context 'with a RC file containing user credentials' do
-      it 'is successful' do
-        # return incomplete creds from env
-        expect(klass).to receive(:get_os_vars_from_env)
-             .and_return({ 'OS_USERNAME' => 'incompleteusername',
-                           'OS_AUTH_URL' => 'incompleteauthurl' })
-        content = "export OS_USERNAME='test'\nexport OS_PASSWORD='abc123'\nexport OS_PROJECT_NAME='test'\nexport OS_AUTH_URL='http://127.0.0.1:5000'\nexport OS_NOT_VALID='notvalid'"
-        expect(File).to receive(:exist?).with("/etc/openstack/puppet/clouds.yaml").and_return(false)
-        expect(File).to receive(:exist?).with("/etc/openstack/puppet/admin-clouds.yaml").and_return(false)
-        expect(File).to receive(:exist?).with("#{ENV['HOME']}/openrc").and_return(true)
-        expect(File).to receive(:open).with("#{ENV['HOME']}/openrc").and_return(StringIO.new(content))
-        expect(klass).to receive(:openstack)
-             .with('project', 'list', '--quiet', '--format', 'csv', ['--long'])
-             .and_return('"ID","Name","Description","Enabled"
-"1cb05cfed7c24279be884ba4f6520262","test","Test tenant",True
-')
-        response = provider.class.request('project', 'list', ['--long'])
-        expect(response.first[:description]).to eq("Test tenant")
-        expect(klass.instance_variable_get(:@credentials).to_env).to eq({
-          'OS_USERNAME'             => 'test',
-          'OS_PASSWORD'             => 'abc123',
-          'OS_PROJECT_NAME'         => 'test',
-          'OS_AUTH_URL'             => 'http://127.0.0.1:5000',
-          'OS_IDENTITY_API_VERSION' => '3'
-        })
-      end
-    end
-
-    context 'with a RC file containing service token credentials' do
-      it 'is successful' do
-        # return incomplete creds from env
-        expect(klass).to receive(:get_os_vars_from_env)
-             .and_return({ 'OS_TOKEN' => 'incomplete' })
-        content = "export OS_TOKEN='test'\nexport OS_ENDPOINT='abc123'\nexport OS_NOT_VALID='notvalid'\n"
-        expect(File).to receive(:exist?).with("/etc/openstack/puppet/clouds.yaml").and_return(false)
-        expect(File).to receive(:exist?).with("/etc/openstack/puppet/admin-clouds.yaml").and_return(false)
-        expect(File).to receive(:exist?).with("#{ENV['HOME']}/openrc").and_return(true)
-        expect(File).to receive(:open).with("#{ENV['HOME']}/openrc").and_return(StringIO.new(content))
-        expect(klass).to receive(:openstack)
-             .with('project', 'list', '--quiet', '--format', 'csv', ['--long'])
-             .and_return('"ID","Name","Description","Enabled"
-"1cb05cfed7c24279be884ba4f6520262","test","Test tenant",True
-')
-        response = klass.request('project', 'list', ['--long'])
-        expect(response.first[:description]).to eq("Test tenant")
-        expect(klass.instance_variable_get(:@credentials).to_env).to eq({
-          'OS_IDENTITY_API_VERSION' => '3',
-          'OS_TOKEN'                => 'test',
-          'OS_ENDPOINT'             => 'abc123',
         })
       end
     end
